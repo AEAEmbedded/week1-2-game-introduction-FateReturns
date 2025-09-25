4df6c6899e6767801cafb2f5cf0ba26b96edd288
FateReturns <avh.deridder@student.han.nl>
2025-09-25 15:51:58 +0200

    Finished
    D

--- main.c
+++ main.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 /*
  * First assignment for creating a game
- * (c) HAN, John van den Hooven REMARK: Change this name to your own
+ * (c) HAN, Ashley de Ridder REMARK: Change this name to your own
  * This is the first part of the game
  * It display's a welcome screen and instructions
  */
 
-int main()
+int main(void)
 {
-   char name[30] = "<name>";
+   char name[30] = "<Ashley's Guessing game>";
    int NumberOfGeusses = 5;
    printf("Welcome to the guessing game!\n");
-   // This is just a start, add alle the lines as mentioned in the assignment
+   // This is just a start, add all the lines as mentioned in the assignment
+   printf("The object of the game is to guess the secret number.\n");
+   printf("You get %d chances for that.\n", NumberOfGeusses);
+   printf("To address you personally, I would like to know your name.\n");
+   printf("What is your name:\n");
+   printf("Great <name>, let's get started.\n");
+   printf("There is a secret number between 0 and 100, what is your first try:");
+
    return 0;
 }
